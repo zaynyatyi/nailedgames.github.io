3df493e0cf36371b517f9dcdae0e2ca31c750083
Heorhiy Kharvat <egor.harvat@dubitlimited.com>
2015-10-24 21:48:48 +0300

    Masonry update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,12 @@
-$('.grid').masonry();
+var $grid = $('.grid').masonry({
+  // disable initial layout
+  isInitLayout: false,
+  //...
+});
+// bind event
+$grid.masonry( 'on', 'layoutComplete', function() {
+  console.log('layout is complete');
+});
+// manually trigger initial layout
+$grid.masonry();
 
